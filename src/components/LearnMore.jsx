9c6f5efaf55c2d77d2b4f8d07eee6660acc53367
Aytan Bayramova <112419467+AytanBayramova@users.noreply.github.com>
2023-04-18 23:27:03 +0400

    Update LearnMore.jsx

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -11,11 +11,9 @@ const LearnMore = () => {
         <h1>Apply AI, Deep Learning and Data Sciece to solve</h1>
         <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
         <Button butonunIcindekiDeyer="Learn more" klassAdi="btn btn-primary"/>
-
     </div>
     </>
   )
- 
 }
 
 export default LearnMore
