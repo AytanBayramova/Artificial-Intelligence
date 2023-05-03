08bf992dcea597157309fc4d2aa0d6de40c5cc19
Aytan Bayramova <112419467+AytanBayramova@users.noreply.github.com>
2023-05-03 23:42:52 +0400

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Button = ({butonunIcindekiDeyer, klassAdi, iconShekli}) => {
   return (
     <button className={klassAdi}>{iconShekli && <img src={iconShekli} />} {butonunIcindekiDeyer}</button>
-
   )
 }
 
