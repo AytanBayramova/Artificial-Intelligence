542faa86d30a81b85fd1ac4bc0db905008d2b304
Aytan Bayramova <112419467+AytanBayramova@users.noreply.github.com>
2023-05-23 23:06:45 +0400

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='p-5 mt-5 text-center bg-success display-1'>About haqqinda</div>
+    <div className='p-5 mt-5 text-center bg-success display-1'>About</div>
   )
 }
 
